c76d435718b0e44f44ce584cd4b1052cd974b77a
Bruno Bortagaray <bruno.bortagaray@justwatch.com>
2020-06-06 13:49:05 +0200

    feat: Create home and create point pages structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import Routes from "./router";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <Routes />;
 }
 
 export default App;
